9d8aa5cbf7edcb31474bd5410078fe0e0b85ce60
hezii <genesis.alvarez@uamaker.nyc>
2019-03-15 21:14:15 -0400

    centered diamond variables
    adds a centered blue diamond using variables

--- shapes5GA.py
+++ shapes5GA.py
@@ -5,7 +5,13 @@ winY = 600
 shapesWin = GraphWin("Shapes 5", winX,winY)
 shapesWin.setCoords(0,0,winX,winY)
 
-
+dSize = 50
+bDia = Polygon(Point(winX/2-dSize,winY/2),
+                Point(winX/2,winY/2+dSize),
+                Point(winX/2+dSize,winY/2),
+                Point(winX/2,winY/2-dSize))
+bDia.setFill(color_rgb (30, 30, 230))
+bDia.draw(shapesWin)
 
 shapesWin.getMouse()
 shapesWin.close()
