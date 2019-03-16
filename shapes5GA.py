d472ca1546b1dd2c0ce526ceecd8be64f3287310
hezii <genesis.alvarez@uamaker.nyc>
2019-03-15 21:16:57 -0400

    red triangle
    adds a red triangle to code

--- shapes5GA.py
+++ shapes5GA.py
@@ -5,6 +5,7 @@ winY = 600
 shapesWin = GraphWin("Shapes 5", winX,winY)
 shapesWin.setCoords(0,0,winX,winY)
 
+
 dSize = 50
 bDia = Polygon(Point(winX/2-dSize,winY/2),
                 Point(winX/2,winY/2+dSize),
@@ -12,6 +13,13 @@ bDia = Polygon(Point(winX/2-dSize,winY/2),
                 Point(winX/2,winY/2-dSize))
 bDia.setFill(color_rgb (30, 30, 230))
 bDia.draw(shapesWin)
+
+rTri = Polygon(Point(50, winY - 5),
+               Point(100, winY - 100),
+               Point(5, winY - 100))
+rTri.setFill(color_rgb(255,0,0))
+rTri.draw(shapesWin)
+
 
 shapesWin.getMouse()
 shapesWin.close()
